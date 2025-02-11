b9f4c09f2b86efcb2cf8773e18a1783609338349
Danilo Acosta <danilo.acostadote@chasacademy.se>
2025-02-12 00:15:17 +0100

    style: enhance navbar layout and remove unused drawer component

--- src/app/ui/navbar/navbar.tsx
+++ src/app/ui/navbar/navbar.tsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import Link from "next/link";
 import NavLinks from "./nav-links";
 import Links from "./types";
-import TemporaryDrawer from "@/components/ui/drawer";
 
 const links: Links[] = [
   {
@@ -29,10 +28,10 @@ export default function Example() {
   const [linkName, setLinkName] = useState("");
 
   return (
-    <header className="bg-white border-b border-gray-300 max-h-[73] min-h-[73] sticky top-0 z-30">
+    <header className="bg-white border-b border-gray-300 min-h-[73] sticky top-0 z-30">
       <nav
         aria-label="Global"
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex flex-col sm:flex-row max-w-7xl items-center justify-between sm:p-6 py-4 lg:px-8 gap-y-2"
       >
         <div className="flex lg:flex-1">
           <Link
@@ -40,7 +39,7 @@ export default function Example() {
             className="-m-1.5 p-1.5 pb-1"
             onClick={() => setLinkName("")}
           >
-            <span className=" absolute top-6">LOGO</span>
+            <span className=" sm:absolute sm:top-6">LOGO</span>
           </Link>
         </div>
         <div className="flex lg:flex-1 lg:hidden">
@@ -48,7 +47,7 @@ export default function Example() {
             <span className=" sr-only">PLACEHOLDER</span>
           </div>
         </div>
-        <div className="hidden sm:flex sm:gap-x-12">
+        <div className="flex sm:gap-x-12">
           <NavLinks
             activeLink={linkName}
             setActiveLink={setLinkName}
@@ -60,63 +59,8 @@ export default function Example() {
             <span className=" sr-only">PLACEHOLDER</span>
           </div>
         </div>
-        {/* <div className="flex sm:hidden">
-          <button
-            type="button"
-            onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-end rounded-md p-2.5 text-gray-700"
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="size-6" />
-          </button>
-        </div> */}
-        <TemporaryDrawer opensFrom="right" links={links} />
+        {/* <TemporaryDrawer opensFrom="right" links={links} /> */}
       </nav>
-      {/* <Dialog
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
-        className="lg:hidden"
-      >
-        <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-30 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <Link
-              href="#home"
-              className="-m-1.5 p-1.5"
-              onClick={() => {
-                setLinkName("");
-                setMobileMenuOpen(false);
-              }}
-            >
-              <span className="">LOGO</span>
-            </Link>
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="size-6" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {links.map((link) => (
-                  <Link
-                    key={link.name}
-                    href={link.href}
-                    onClick={() => setMobileMenuOpen(false)}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                  >
-                    {link.name}
-                  </Link>
-                ))}
-              </div>
-            </div>
-          </div>
-        </DialogPanel>
-      </Dialog> */}
     </header>
   );
 }
