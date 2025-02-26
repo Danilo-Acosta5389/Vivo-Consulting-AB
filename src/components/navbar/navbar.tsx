768291bb36b1a251a81d3b63e605aa8fe13f6fe7
Danilo Acosta <danilo.acostadote@chasacademy.se>
2025-02-26 16:57:49 +0100

    refactor: clean up Hero and Navbar components, update logo implementation

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import NavLinks from "./nav-links";
 import Links from "./types";
 import { useNavlinkContext } from "@/context/navlink-context";
+import Image from "next/image";
 
 const links: Links[] = [
   {
@@ -27,21 +28,27 @@ export default function Navbar() {
   const { activeLink, setActiveLink } = useNavlinkContext();
 
   return (
-    <header className="bg-white border-b border-gray-300 min-h-[73] sticky top-0 z-30">
+    <header className="bg-white border-b border-gray-300 min-h-[73px] sticky top-0 z-30 flex flex-col justify-center items-center ">
       <nav
         aria-label="Global"
-        className="mx-auto flex flex-col sm:flex-row max-w-7xl items-center justify-between sm:p-6 py-4 lg:px-8 gap-y-2"
+        className="mx-auto flex flex-col md:flex-row max-w-7xl w-full items-center justify-between sm:p-6 py-4 lg:px-8 gap-y-2"
       >
         <div className="flex lg:flex-1">
           <Link
-            href="#home"
             className="-m-1.5 p-1.5 pb-1"
+            href="#home"
             onClick={() => setActiveLink("")}
           >
-            <span className=" sm:absolute sm:top-6">LOGO</span>
+            <Image
+              src={"/vivo-logo.svg"}
+              width={150}
+              height={50}
+              alt="Company logo"
+              className=" md:absolute md:top-4"
+            />
           </Link>
         </div>
-        <div className="flex lg:flex-1 sm:hidden">
+        <div className="flex lg:flex-1 md:hidden">
           <div className="-m-1.5 p-1.5">
             <span className=" sr-only">PLACEHOLDER</span>
           </div>
@@ -58,7 +65,6 @@ export default function Navbar() {
             <span className=" sr-only">PLACEHOLDER</span>
           </div>
         </div>
-        {/* <TemporaryDrawer opensFrom="right" links={links} /> */}
       </nav>
     </header>
   );
