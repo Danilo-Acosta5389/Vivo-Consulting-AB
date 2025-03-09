4abd329363c15c06fcff3388b47bd3d17c536561
Danilo Acosta <danilo.acostadote@chasacademy.se>
2025-03-09 01:09:19 +0100

    refactor: enhance emailSender function to send confirmation email to sender

--- src/utils/email-sender.ts
+++ src/utils/email-sender.ts
@@ -37,6 +37,7 @@ const recipients: Mail.Address[] = [
 
 export default async function emailSender(dto: SendEmailDTO) {
   try {
+    // Email to owner of site
     await transporter.sendMail({
       from: sender,
       to: recipients,
@@ -53,6 +54,66 @@ export default async function emailSender(dto: SendEmailDTO) {
       <p style="max-width:500px;">${dto.message}</p>
       </body>`.trim(),
     });
+
+    // Email to sender
+    await transporter.sendMail({
+      from: sender,
+      to: dto.email,
+      subject: "Tack för ditt meddelande",
+      text: "Vi har mottagit ditt meddelande och återkommer så snart vi kan",
+      html: `
+      <body
+    style="max-width: 450px; width: full; min-height: 700px; padding-bottom: 20px; border: 20px solid rgb(233, 233, 233)"
+  >
+    <div
+      style="
+        margin: 10px;
+        display: block;
+        font-family: helvetica;
+        font-weight: bold;
+      "
+    >
+      <h3>Hej, ${dto.name}</h3>
+      <p style="font-size: medium; font-weight: normal">
+        Tack för ditt meddelande. <br />Du kommer att få ett svar från oss inom
+        1-3 dagar. <br /><br />Varma hälsningar<span style="font-weight: bolder"
+          >,<br />
+          Vivo Nurse</span
+        >
+      </p>
+      <div style="margin-top: 2rem">
+        Title
+        <div
+          style="
+            margin-top: 0.5rem;
+            margin:auto;
+            padding: 10px;
+            min-height: 200px;
+            background-color: rgb(233, 233, 233);
+          "
+        >
+            <div style="margin-bottom: 0.5rem; line-height: 1.5rem;">
+              <span style="font-weight: bold">Namn:</span> <br />
+              <span style="font-weight: lighter">${dto.name}</span>
+            </div>
+            <div style="margin-bottom: 0.5rem; line-height: 1.5rem;">
+              <span style="font-weight: bold">E-post:</span> <br />
+              <span style="font-weight: lighter">${dto.email}</span>
+            </div>
+            <div style="margin-bottom: 0.5rem; line-height: 1.5rem;">
+              <span style="font-weight: bold">Telefonnummer:</span> <br />
+              <span style="font-weight: lighter">${dto.number}</span>
+            </div>
+            <div style="margin-bottom: 0.5rem;">
+              <div style="font-weight: bold; margin-bottom:5px;">Meddelande:</div>
+              <span style="font-weight: lighter; line-break:normal;">${dto.message}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>`.trim(),
+    });
     return { success: true, error: null };
   } catch (error) {
     return {
@@ -61,3 +122,20 @@ export default async function emailSender(dto: SendEmailDTO) {
     };
   }
 }
+
+// Confirmation Email image - WIP
+// <img src="https://img.freepik.com/premium-vector/template-logo-design-trader-seller_1089435-23.jpg"
+//  alt="Vivo Consulting Logo"
+//  width="200"
+//  height="100"
+//  style="display: block; margin: 0 auto;"/>
+
+// <div
+//   style="
+//     width: 85%;
+//     border: 1px solid rgb(219, 219, 219);
+//     margin-right: auto;
+//     margin-left: auto;
+//     display: block;
+//   "
+// ></div>
